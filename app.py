de20b4fd78f1acf58ce256fdce5a5556f1c34579
pablitus03 <91649504+pablitus03@users.noreply.github.com>
2023-08-24 07:19:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ st.write ("confio en que puuedo entender")
 image = Image.open('nyan_cat.jpg')
 
 st.image(image, caption= 'interfaces')
+
+texto =st.text_imput("pa pe pi po pu","mia mie mii mio miu")
+st.write( "el texto es", texto)
