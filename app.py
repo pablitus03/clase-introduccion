eba4f230abe833b225865ecdb812ce943bdc2c6f
pablitus03 <91649504+pablitus03@users.noreply.github.com>
2023-08-24 07:08:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from PIL import image
 
 st.title("miau de pablitus03")
+
+st.header ("aqui comienza mi historia")
+st.write ("confio en que puuedo entender")
+image = image.open(
