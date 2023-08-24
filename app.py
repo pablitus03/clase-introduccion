2fcbd94e128d23f06886c83e4c8b9a93cb552baf
pablitus03 <91649504+pablitus03@users.noreply.github.com>
2023-08-24 07:12:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ st.header ("aqui comienza mi historia")
 st.write ("confio en que puuedo entender")
 image = image.open("nyan_cat.jpg")
 
-st.image(image, caption="interfaces")
+st.image(image, caption= "interfaces")
